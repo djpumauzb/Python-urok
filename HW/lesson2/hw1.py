a5613d18ad07dc28469bfe6fcb5e2c5b8711fd9c
Sirojiddin <sirojboy@inbox.ru>
2023-11-04 20:17:58 +0500

    add hw to lssn2

---
+++ HW/lesson2/hw1.py
@@ -0,0 +1,11 @@
+'''
+Na stole lejat "n" monetok. Nekotorie iz nix lejat vverx reshkoy,
+a nekotrorie  - gerbom. Opredelite minimalnoe chislo monetok, 
+kotorie nujno perevernut', chtobi vse monetki bili povernuti
+vverx odnoy i toy je storonoy. Vivedite minimalnoe kolichestvo monet,
+kotorie nujno pervernut'
+
+5 -> 1 0 1 1 0
+2
+'''
+
