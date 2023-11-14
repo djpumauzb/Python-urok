9627264af7cf6ff0bd48f36d617fb5fb4fd6c7f3
Sirojiddin <sirojboy@inbox.ru>
2023-11-14 10:31:58 +0500

    add tsk2 sem2

---
+++ Seminar/lesson2/tsk2.py
@@ -0,0 +1,8 @@
+'''
+Dano naturalnoe chislo A > . Opredelite, kakim po
+schetu chislom Fibonachi ono yavlaesya, to est'
+vivedite takoe chislo n, chto f(n)=A. Esli A ne
+yavlaetsya chislom fibonachim vivedite chislo -1.
+5 => 6
+'''
+
